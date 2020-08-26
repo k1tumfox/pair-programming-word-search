7e115506f042aa91fc1ace08e66e54beaad629da
Tenyson Kwon <67251061+k1tumfox@users.noreply.github.com>
2020-08-26 19:22:13 -0400

    Update wordsearch.js

--- wordsearch.js
+++ wordsearch.js
@@ -1,46 +1,41 @@
-const wordSearch = (letters, word) => { 
-    if(letters.length === 0) {
-        return false;
-    }
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-    const verticalMatrix = transpose(letters);
-    const verticalJoin = verticalMatrix.map(ls => ls.join(''))
-    for (l of verticalJoin) {
-        if (l.includes(word)) return true
-    }    
-    return false;
-}
 const transpose = function(matrix) {
-    // Put your solution here
-    let newArray = [];
-    let maxCols = matrix[0].length;
-    for(let x = 0; x < maxCols; x++) {
-      newArray[x] = [];
-    }
-    if(matrix.length == 1)
-    {
-      for(let i = 0; i < matrix[0].length; i++) {
-        newArray[i].push(matrix[0][i]);
+  const newArr = [];
+  for (let row = 0; row < matrix[0].length; row++) {
+    const newRow = [];
+    for (let col = 0; col < matrix.length; col++) {
+      if (matrix[col][row] === undefined) {
+        newRow.push(' ');
+      } else {
+        newRow.push(matrix[col][row]);
       }
-      return newArray;
-    }  
-    recurFunction(matrix, newArray, maxCols, 0);
-    return newArray;
-  };
-  const recurFunction = (matrix, outputArray, maxCols, colIndex) => {
-    if(colIndex === matrix.length) {
-      return;
     }
-    if(colIndex === maxCols) {
-      return;
-    }
-    for(let x = 0; x < matrix.length; x++) {
-      outputArray[colIndex].push(matrix[x][colIndex]);
-    }
-    recurFunction(matrix, outputArray, maxCols, ++colIndex);
-  };
+    newArr.push(newRow);
+  }
+  return newArr;
+};
 
-module.exports = wordSearch
+const reverse = function(arr) {
+  arr.reverse();
+  arr.map(val => val.reverse());
+};
+
+const wordSearch = (letters, word) => {
+  let horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  let lettersTransposed = transpose(letters);
+  horizontalJoin = lettersTransposed.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  reverse(letters);
+  horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  
+  return false;
+};
+
+module.exports = wordSearch;
